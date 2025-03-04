14b2e90060dc8b0afdd5f8f237106cdf71dcdd54
ArajaID <arajaid@gmail.com>
2025-03-04 09:47:51 +0700

    feat ticket management

--- src/components/ui/DataTable/DataTable.tsx
+++ src/components/ui/DataTable/DataTable.tsx
@@ -26,6 +26,8 @@ interface PropTypes {
     data: Record<string, unknown>[];
     onClickButtonTopContent?: () => void;
     renderCell: (item: Record<string, unknown>, columnKey: Key) => ReactNode;
+    showLimit?: boolean;
+    showSearch?: boolean;
     totalPages: number;
 }
 
@@ -48,6 +50,8 @@ const DataTable = (props: PropTypes) => {
         isLoading,
         onClickButtonTopContent, 
         renderCell,
+        showLimit = true,
+        showSearch = true,
         totalPages,
 
     } = props;
@@ -55,14 +59,16 @@ const DataTable = (props: PropTypes) => {
     const TopContent = useMemo(() => {
         return (
             <div className="flex flex-col-reverse items-start justify-between gap-y-4 lg:flex-row lg:items-center">
-                <Input 
-                isClearable 
-                className="w-full sm:max-w-[24%]" 
-                placeholder="Search by name" 
-                startContent={<CiSearch />}
-                onClear={handleClearSearch}
-                onChange={handleSearch}
-                />
+                {showSearch && (
+                    <Input 
+                        isClearable 
+                        className="w-full sm:max-w-[24%]" 
+                        placeholder="Search by name" 
+                        startContent={<CiSearch />}
+                        onClear={handleClearSearch}
+                        onChange={handleSearch}
+                    />
+                )}
                 { buttonTopContentLabel && <Button color="danger" onPress={onClickButtonTopContent}>
                     { buttonTopContentLabel }
                 </Button> }
@@ -78,21 +84,23 @@ const DataTable = (props: PropTypes) => {
     const BottomContent = useMemo(() => {
         return (
             <div className="flex items-center justify-center lg:justify-between">
-                <Select 
-                    className="hidden max-w-36 lg:block" 
-                    size="md" 
-                    selectedKeys={[`${currentLimit}`]}
-                    selectionMode="single"
-                    onChange={handleChangeLimit}
-                    startContent={<p className="text-small">Show:</p>}
-                    disallowEmptySelection
-                    >
-                        {LIMIT_LISTS.map((item) => (
-                            <SelectItem key={item.value} value={item.value}>
-                                { item.label }
-                            </SelectItem>
-                        ))}
-                </Select>
+                {showLimit && (
+                    <Select 
+                        className="hidden max-w-36 lg:block" 
+                        size="md" 
+                        selectedKeys={[`${currentLimit}`]}
+                        selectionMode="single"
+                        onChange={handleChangeLimit}
+                        startContent={<p className="text-small">Show:</p>}
+                        disallowEmptySelection
+                        >
+                            {LIMIT_LISTS.map((item) => (
+                                <SelectItem key={item.value} value={item.value}>
+                                    { item.label }
+                                </SelectItem>
+                            ))}
+                    </Select>
+                )}
 
                { totalPages > 1 && (
                     <Pagination 
